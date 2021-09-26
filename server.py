9d7c6e39196d92a15ffe8c8519abd0b22beb4013
David Bystřický <61781749+ykcirtsyb@users.noreply.github.com>
2021-09-26 17:29:44 +0200

    added hw check and some html content

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-__version__ = "1.0.0"
+__version__ = "1.0.1"
+
+DEBUG = False
+
 
 
 ################################ REMOTE USE ################################
@@ -20,6 +23,23 @@ from optparse import OptionParser
 import os, platform
 
 ###################################################################################################
+
+def is_rasPi():
+    if DEBUG:
+        return True
+
+    if platform.system() == "Windows":
+        return False
+    else:
+        ret = os.uname().machine.lower().rfind("armv")
+        return True if ret != -1 else False
+
+###################################################################################################
+
+def is_root():
+    return os.geteuid() == 0
+
+###################################################################################################
 ###################################################################################################
 
 class MyHTTPServer(BaseHTTPRequestHandler):
@@ -28,14 +48,48 @@ class MyHTTPServer(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-        if self.path == "/shutdown":
-            self.wfile.write(b"OK, Bye")
-            os.popen("sudo shutdown now")
+        html_text = f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ShutdownServer</title>
+</head>
+<body>
+    <section style = "text-align: center;">
+        {'<strong>KO</strong>' if self.path != "/test" and self.path != "/shutdown" else ''}
+        {'<strong>OK</strong>' if self.path == "/test" else ''}
+        {'<strong>OK, Bye</strong>' if self.path == "/shutdown" else ''}
+        {'<br/>' if self.path == "/shutdown" and is_rasPi() else ''}
+        {'<br/>' if self.path == "/shutdown" and is_rasPi() else ''}
+        {'<strong>After a while, unplug the power adapter from the wall!!</strong>' if self.path == "/shutdown" and is_rasPi() else ''}
+    </section>
+</body>
+</html>
 
-        if self.path == "/test":
-            self.wfile.write(b"OK")
+"""
+
+        user_agent = self.headers.get("User-Agent").split('/')[0]      # curl, wget, Mozilla, Chrome
+
+        if user_agent.lower() in ["curl", "wget"]:
+            if self.path == "/shutdown":
+                self.wfile.write(b'OK, Bye.\n')
+                if is_rasPi():
+                    self.wfile.write(b'After a while, unplug the power adapter from the wall!!\n')
+                self.wfile.write(b'\r')
+
+            elif self.path == "/test":
+                self.wfile.write(b'OK\n\r')
+            else:
+                self.wfile.write(b'KO\n\r')
         else:
-            self.wfile.write(b"KO")
+            self.wfile.write(bytes(html_text, 'utf-8'))
+
+
+        if self.path == "/shutdown":
+            os.popen("sudo shutdown now") if DEBUG != True else print("Shutting Down")
 
 ###################################################################################################
 ###################################################################################################
@@ -50,7 +104,7 @@ def httpMethod(httpServerPort):
 
     webServer = HTTPServer((httpHostName, httpServerPort), MyHTTPServer)
 
-    print("[INFO] ShutdownServer started:")
+    print("ShutdownServer started:")
     port = f":{httpServerPort if httpServerPort != 80 else ''}"
     print(f"\thttp://{httpHostName}{port}")
 
@@ -60,12 +114,7 @@ def httpMethod(httpServerPort):
         pass
 
     webServer.server_close()
-    print("[INFO] ShutdownServer stopped.")
-
-###################################################################################################
-
-def is_root():
-    return os.geteuid() == 0
+    print("ShutdownServer stopped.")
 
 ###################################################################################################
 
